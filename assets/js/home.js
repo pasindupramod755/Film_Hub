9a35e53af6b62215dcf3b7deb1b82ccd8dae7b6b
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-04 09:55:07 +0530

    Add View Page navBar

--- assets/js/home.js
+++ assets/js/home.js
@@ -21,13 +21,13 @@ document.getElementById("navBar").innerHTML = `
 
             <div class="w-full md:block md:w-auto hidden transition-all duration-300 ease-in-out" id="mobile-menu">
                 <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                    <li><a href="#"
+                    <li><a href="index.html"
                             class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                             aria-current="page">Home</a></li>
                     <li><a href="#"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About</a>
                     </li>
-                    <li><a href="#"
+                    <li><a href="view.html"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Services</a>
                     </li>
                     <li><a href="#"
